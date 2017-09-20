7a411a81bb457097b09617002efff4ed70b0f9d4
MrPhil <mrphil@mrphilgames.com>
2017-09-20 00:36:05 -0500

    Step 2

---
+++ CatLikeCoding-CubeSphere/Assets/CircleGizmo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleGizmo : MonoBehaviour
+{
+
+	public int resolution = 10;
+
+	private void OnDrawGizmosSelected()
+	{
+		float step = 2.0f / resolution;
+		for (int i = 0; i <= resolution; i++)
+		{
+			ShowPoint(i * step - 1.0f, -1.0f);
+			ShowPoint(i * step - 1.0f, 1.0f);
+		}
+
+		for (int i = 0; i <= resolution; i++)
+		{
+			ShowPoint(-1.0f, i * step - 1.0f);
+			ShowPoint(1.0f, i * step - 1.0f);
+		}
+	}
+
+	private void ShowPoint(float x, float y)
+	{
+		Vector2 square = new Vector2(x, y);
+		Vector2 circle = square.normalized;
+
+		Gizmos.color = Color.black;
+		Gizmos.DrawSphere(square, 0.025f);
+
+		Gizmos.color = Color.white;
+		Gizmos.DrawSphere(circle, 0.025f);
+
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(square, circle);
+
+		Gizmos.color = Color.black;
+		Gizmos.DrawLine(circle, Vector2.zero);
+	}
+}
