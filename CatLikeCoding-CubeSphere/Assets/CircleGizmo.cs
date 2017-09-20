46ee20fde01514bbace9b075d7d85ad39e768fce
MrPhil <mrphil@mrphilgames.com>
2017-09-20 00:52:18 -0500

    Import Step 2

--- CatLikeCoding-CubeSphere/Assets/CircleGizmo.cs
+++ CatLikeCoding-CubeSphere/Assets/CircleGizmo.cs
@@ -1,30 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CircleGizmo : MonoBehaviour
-{
+public class CircleGizmo : MonoBehaviour {
 
 	public int resolution = 10;
 
-	private void OnDrawGizmosSelected()
-	{
-		float step = 2.0f / resolution;
-		for (int i = 0; i <= resolution; i++)
-		{
-			ShowPoint(i * step - 1.0f, -1.0f);
-			ShowPoint(i * step - 1.0f, 1.0f);
+	private void OnDrawGizmosSelected () {
+		float step = 2f / resolution;
+		for (int i = 0; i <= resolution; i++) {
+			ShowPoint(i * step - 1f, -1f);
+			ShowPoint(i * step - 1f, 1f);
 		}
-
-		for (int i = 0; i <= resolution; i++)
-		{
-			ShowPoint(-1.0f, i * step - 1.0f);
-			ShowPoint(1.0f, i * step - 1.0f);
+		for (int i = 1; i < resolution; i++) {
+			ShowPoint(-1f, i * step - 1f);
+			ShowPoint(1f, i * step - 1f);
 		}
 	}
 
-	private void ShowPoint(float x, float y)
-	{
+	private void ShowPoint (float x, float y) {
 		Vector2 square = new Vector2(x, y);
 		Vector2 circle = square.normalized;
 
@@ -37,7 +29,7 @@ public class CircleGizmo : MonoBehaviour
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawLine(square, circle);
 
-		Gizmos.color = Color.black;
+		Gizmos.color = Color.gray;
 		Gizmos.DrawLine(circle, Vector2.zero);
 	}
 }
