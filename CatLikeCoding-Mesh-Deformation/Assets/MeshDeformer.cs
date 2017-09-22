780616d143cf994bdc4bb07ff2940f647caaee31
MrPhil <mrphil@mrphilgames.com>
2017-09-21 23:42:48 -0500

    Step 3

--- CatLikeCoding-Mesh-Deformation/Assets/MeshDeformer.cs
+++ CatLikeCoding-Mesh-Deformation/Assets/MeshDeformer.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
-public class MeshDeformer : MonoBehaviour {
+public class MeshDeformer : MonoBehaviour
+{
 
 	Mesh deformingMesh;
 	Vector3[] originalVertices, displacedVertices;
 	Vector3[] vertexVelocities;
 
-	void Start () {
+	void Start()
+	{
 		deformingMesh = GetComponent<MeshFilter>().mesh;
 		originalVertices = deformingMesh.vertices;
 		displacedVertices = new Vector3[originalVertices.Length];
-		for (int i = 0; i < originalVertices.Length; i++) {
+		for (int i = 0; i < originalVertices.Length; i++)
+		{
 			displacedVertices[i] = originalVertices[i];
 		}
 		vertexVelocities = new Vector3[originalVertices.Length];
+	}
+
+	public void AddDeformingForce(Vector3 point, float force)
+	{
+		Debug.DrawLine(Camera.main.transform.position, point);
 	}
 }
