b66c0e140b345bb8353b1db9d4a567f865e84ac9
MrPhil <mrphil@mrphilgames.com>
2017-09-24 15:13:06 -0500

    Step 6

--- CatLikeCoding-Mesh-Deformation/Assets/MeshDeformer.cs
+++ CatLikeCoding-Mesh-Deformation/Assets/MeshDeformer.cs
@@ -10,6 +10,7 @@ public class MeshDeformer : MonoBehaviour
 	Mesh deformingMesh;
 	Vector3[] originalVertices, displacedVertices;
 	Vector3[] vertexVelocities;
+	float uniformScale = 1f;
 
 	void Start()
 	{
@@ -25,6 +26,7 @@ public class MeshDeformer : MonoBehaviour
 
 	void Update()
 	{
+		uniformScale = transform.localScale.x;
 		for (int i = 0; i < displacedVertices.Length; i++)
 		{
 			UpdateVertex(i);
@@ -37,14 +39,17 @@ public class MeshDeformer : MonoBehaviour
 	{
 		Vector3 velocity = vertexVelocities[i];
 		Vector3 displacement = displacedVertices[i] - originalVertices[i];
+		displacement *= uniformScale;
 		velocity -= displacement * springForce * Time.deltaTime;
 		velocity *= 1f - damping * Time.deltaTime;
 		vertexVelocities[i] = velocity;
-		displacedVertices[i] += velocity * Time.deltaTime;
+		displacedVertices[i] += velocity * (Time.deltaTime / uniformScale); 
 	}
 
 	public void AddDeformingForce(Vector3 point, float force)
 	{
+		point = transform.InverseTransformPoint(point);
+
 		for (int i = 0; i < displacedVertices.Length; i++)
 		{
 			AddForceToVertex(i, point, force);
@@ -54,6 +59,7 @@ public class MeshDeformer : MonoBehaviour
 	void AddForceToVertex(int i, Vector3 point, float force)
 	{
 		Vector3 pointToVertex = displacedVertices[i] - point;
+		pointToVertex *= uniformScale;
 		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
 		float velocity = attenuatedForce * Time.deltaTime;
 		vertexVelocities[i] += pointToVertex.normalized * velocity;
