0c6621de2f88b57a9474ab1ed6adbd52028c754c
MrPhil <mrphil@mrphilgames.com>
2017-09-24 15:48:25 -0500

    Import 1

--- CatLikeCoding-Hex-Map-1/Assets/Scripts/HexMetrics.cs
+++ CatLikeCoding-Hex-Map-1/Assets/Scripts/HexMetrics.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class HexMetrics
-{
+public static class HexMetrics {
 
 	public const float outerRadius = 10f;
+
 	public const float innerRadius = outerRadius * 0.866025404f;
 
 	public static Vector3[] corners = {
