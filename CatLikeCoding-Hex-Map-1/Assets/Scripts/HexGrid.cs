aa57510fe0603f7a098dabadd40a72ecdf90bd40
MrPhil <mrphil@mrphilgames.com>
2017-09-25 09:30:19 -0500

    Step 2

---
+++ CatLikeCoding-Hex-Map-1/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+
+	public int width = 6;
+	public int height = 6;
+
+	public HexCell cellPrefab;
+	public Text cellLabelPrefab;
+
+	HexCell[] cells;
+	Canvas gridCanvas;
+
+	void Awake()
+	{
+		gridCanvas = GetComponentInChildren<Canvas>();
+		cells = new HexCell[height * width];
+
+		for (int z = 0, i = 0; z < height; z++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				CreateCell(x, z, i++);
+			}
+		}
+	}
+
+	void CreateCell(int x, int z, int i)
+	{
+		Vector3 position;
+		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+		position.y = 0;
+		position.z = z * (HexMetrics.innerRadius * 1.5f);
+
+		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+		cell.transform.SetParent(transform, false);
+		cell.transform.localPosition = position;
+
+		Text label = Instantiate<Text>(cellLabelPrefab);
+		label.rectTransform.SetParent(gridCanvas.transform, false);
+		label.rectTransform.anchoredPosition =
+			new Vector2(position.x, position.z);
+		label.text = x.ToString() + "\n" + z.ToString();
+	}
+}
