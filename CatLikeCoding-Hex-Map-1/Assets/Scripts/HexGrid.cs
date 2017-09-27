d790faf287d0b2c1d2924d4bdae2f18220188fe2
MrPhil <mrphil@mrphilgames.com>
2017-09-27 00:50:45 -0500

    Step 6

--- CatLikeCoding-Hex-Map-1/Assets/Scripts/HexGrid.cs
+++ CatLikeCoding-Hex-Map-1/Assets/Scripts/HexGrid.cs
@@ -8,7 +8,6 @@ public class HexGrid : MonoBehaviour
 	public int height = 6;
 
 	public Color defaultColor = Color.white;
-	public Color touchedColor = Color.magenta;
 
 	public HexCell cellPrefab;
 	public Text cellLabelPrefab;
@@ -39,31 +38,13 @@ public class HexGrid : MonoBehaviour
 		hexMesh.Triangulate(cells);
 	}
 
-	void Update()
-	{
-		if (Input.GetMouseButton(0))
-		{
-			HandleInput();
-		}
-	}
-
-	void HandleInput()
-	{
-		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast(inputRay, out hit))
-		{
-			TouchCell(hit.point);
-		}
-	}
-
-	void TouchCell(Vector3 position)
+	public void ColorCell(Vector3 position, Color color)
 	{
 		position = transform.InverseTransformPoint(position);
 		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
 		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
 		HexCell cell = cells[index];
-		cell.color = touchedColor;
+		cell.color = color;
 		hexMesh.Triangulate(cells);
 	}
 
