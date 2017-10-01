5788c37890491c2fcac2dd1efca4f7f8aefdc726
MrPhil <mrphil@mrphilgames.com>
2017-10-01 17:27:05 -0500

    Step 3 (but something is wrong)

--- CatLikeCoding-Hex-Map-2/Assets/Scripts/HexMesh.cs
+++ CatLikeCoding-Hex-Map-2/Assets/Scripts/HexMesh.cs
@@ -50,19 +50,34 @@ public class HexMesh : MonoBehaviour
 	void Triangulate(HexDirection direction, HexCell cell)
 	{
 		Vector3 center = cell.transform.localPosition;
-		AddTriangle(
-			center,
-			center + HexMetrics.GetFirstCorner(direction),
-			center + HexMetrics.GetSecondCorner(direction)
-		);
+		Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+		Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+		AddTriangle(center, v1, v2);
+		AddTriangleColor(cell.color);
+
+		Vector3 bridge = HexMetrics.GetBridge(direction);
+		Vector3 v3 = v1 + bridge;
+		Vector3 v4 = v2 + bridge;
+
+		AddQuad(v1, v2, v3, v4);
+
 		HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
 		HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
 		HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
-		AddTriangleColor(
-			cell.color,
-			(cell.color + prevNeighbor.color + neighbor.color) / 3f,
-			(cell.color + neighbor.color + nextNeighbor.color) / 3f
-		);
+
+		Color bridgeColor = (cell.color + neighbor.color) * 0.5f;
+		AddQuadColor(cell.color, bridgeColor);
+
+		AddTriangle(v1, center + HexMetrics.GetFirstCorner(direction), v3);
+		AddTriangleColor(cell.color,
+		                 (cell.color + prevNeighbor.color + neighbor.color) / 3f,
+		                 bridgeColor);
+
+		AddTriangle(v2, v4, center + HexMetrics.GetSecondCorner(direction));
+		AddTriangleColor(cell.color,
+		                 bridgeColor,
+		                 (cell.color + neighbor.color + nextNeighbor.color) / 3f);
 	}
 
 	void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
@@ -88,5 +103,36 @@ public class HexMesh : MonoBehaviour
 		colors.Add(c1);
 		colors.Add(c2);
 		colors.Add(c3);
+	}
+
+	void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+	{
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		vertices.Add(v4);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 3);
+	}
+
+	void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
+	{
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+		colors.Add(c4);
+	}
+
+	void AddQuadColor(Color c1, Color c2)
+	{
+		colors.Add(c1);
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c2);
 	}
 }
--- CatLikeCoding-Hex-Map-2/Assets/Scripts/HexMetrics.cs
+++ CatLikeCoding-Hex-Map-2/Assets/Scripts/HexMetrics.cs
@@ -7,6 +7,10 @@ public static class HexMetrics
 
 	public const float innerRadius = outerRadius * 0.866025404f;
 
+	public const float solidFactor = 0.75f;
+
+	public const float blendFactor = 1f - solidFactor;
+
 	static Vector3[] corners = {
 		new Vector3(0f, 0f, outerRadius),
 		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
@@ -19,11 +23,27 @@ public static class HexMetrics
 
 	public static Vector3 GetFirstCorner(HexDirection direction)
 	{
-		return corners[(int)direction];
+		return corners[(int)direction] * solidFactor;
 	}
 
 	public static Vector3 GetSecondCorner(HexDirection direction)
 	{
-		return corners[(int)direction + 1];
+		return corners[(int)direction + 1] * solidFactor;
+	}
+
+	public static Vector3 GetFirstSolidCorner(HexDirection direction)
+	{
+		return corners[(int)direction] * solidFactor;
+	}
+
+	public static Vector3 GetSecondSolidCorner(HexDirection direction)
+	{
+		return corners[(int)direction + 1] * solidFactor;
+	}
+
+	public static Vector3 GetBridge(HexDirection direction)
+	{
+		return (corners[(int)direction] + corners[(int)direction + 1]) *
+		0.5f * blendFactor;
 	}
 }
